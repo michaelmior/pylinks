29e818d2274ac7a5c5aa1bb9a3f01151743ff400
Michael Mior <mike@joinbunch.com>
2012-11-25 12:31:59 -0500

    Switch to non-secure URLs for S3

--- pylinks/settings.py
+++ pylinks/settings.py
@@ -185,6 +185,8 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_SECURE_URLS = False
+    AWS_QUERYSTRING_AUTH = False
 
     MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
     MEDIA_URL = '//s3.amazonaws.com/%s/%s/' % \
