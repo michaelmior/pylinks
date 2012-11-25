e7057eb7e7e442b2334ae8182cfe0862f038a8b3
Michael Mior <mike@joinbunch.com>
2012-11-25 18:45:51 -0500

    Add recent and popular links to menu

---
+++ pylinks/main/templatetags/menu_li.py
@@ -0,0 +1,42 @@
+from django.template import Library
+from django.template.defaulttags import URLNode, url
+from django.utils.html import escape, mark_safe
+
+register = Library()
+
+
+class MenuLINode(URLNode):
+    def render(self, context):
+        # Pull out the match and hijack asvar
+        # to be used for the link title
+        match = context['request'].resolver_match
+        if self.asvar:
+            title = escape(self.asvar.strip('"\''))
+        else:
+            title = match.url_name
+
+        # Reset asvar and render to get the URL
+        self.asvar = None
+        menu_url = super(MenuLINode, self).render(context)
+
+        # Check if we're on the defined page
+        if str(self.view_name).strip('"\'') == match.url_name:
+            active_class = ' class="active"'
+        else:
+            active_class = ''
+
+        return mark_safe('<li%s><a href="%s">%s</a></li>' % \
+                (active_class, menu_url, title))
+
+
+@register.tag
+def menu_li(parser, token, node_cls=MenuLINode):
+    """
+    Add a menu <li> for Twitter Bootstrap, checking
+    adding the "active" class as needed.
+    """
+    node_instance = url(parser, token)
+    return node_cls(view_name=node_instance.view_name,
+        args=node_instance.args,
+        kwargs=node_instance.kwargs,
+        asvar=node_instance.asvar)
