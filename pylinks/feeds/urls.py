18691acb4698d5a3c4bef9bdd6ac66e76c6638f1
Michael Mior <mike@joinbunch.com>
2012-11-25 19:23:42 -0500

    Add feed links to each relevant page
    This also swaps the "macro" templatetag for the much simpler "captureas"
    which is flexible enough for our purposes.

--- pylinks/feeds/urls.py
+++ pylinks/feeds/urls.py
@@ -3,7 +3,10 @@ from pylinks.feeds import feeds
 
 
 urlpatterns = patterns('',
-    url(r'^category/(?P<slug>.*)/', feeds.CategoryFeed()),
-    url(r'^recent/', feeds.RecentFeed()),
-    url(r'^popular/', feeds.PopularFeed()),
+    url(r'^category/(?P<slug>.*)/', feeds.CategoryFeed(),
+      name='feed_category'),
+    url(r'^recent/', feeds.RecentFeed(),
+      name='feed_recent'),
+    url(r'^popular/', feeds.PopularFeed(),
+      name='feed_popular'),
 )
