0b1cdfac668b15ab9d48b1eb8a4ac4bff7b8c98e
Michael Mior <mike@joinbunch.com>
2012-12-04 23:35:28 -0500

    Fix check for valid resolver_match

--- pylinks/main/templatetags/menu_li.py
+++ pylinks/main/templatetags/menu_li.py
@@ -9,19 +9,18 @@ class MenuLINode(URLNode):
     def render(self, context):
         # Pull out the match and hijack asvar
         # to be used for the link title
-        request = context.get('request')
+        match = getattr(context.get('request'), 'resolver_match', None)
         if self.asvar:
             title = escape(self.asvar.strip('"\''))
-        elif request:
-            title = request.resolver_match.url_name
+        elif match:
+            title = match.url_name
 
         # Reset asvar and render to get the URL
         self.asvar = None
         menu_url = super(MenuLINode, self).render(context)
 
         # Check if we're on the defined page
-        if request and str(self.view_name).strip('"\'') == \
-                request.resolver_match.url_name:
+        if match and str(self.view_name).strip('"\'') == match.url_name:
             active_class = ' class="active"'
         else:
             active_class = ''
