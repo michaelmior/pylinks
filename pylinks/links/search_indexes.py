d3b5e8dbd588724767c306d3446ef15e4f76261b
Michael Mior <mike@joinbunch.com>
2012-12-26 21:28:52 -0500

    Add title to search index

--- pylinks/links/search_indexes.py
+++ pylinks/links/search_indexes.py
@@ -4,6 +4,7 @@ from pylinks.links.models import Link
 
 class LinkIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title', boost=1.125)
 
     def get_model(self):
         return Link
