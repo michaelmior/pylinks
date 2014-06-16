5edd4de55a21a3f99e23dcf90469ea4859c2cbf4
Michael Mior <mmior@uwaterloo.ca>
2014-06-16 12:03:33 -0400

    Fix static files settings

--- pylinks/settings.py
+++ pylinks/settings.py
@@ -48,32 +48,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -200,5 +174,10 @@ if os.environ.get('AWS_ACCESS_KEY_ID'):
     STATIC_URL = '//s3.amazonaws.com/%s/%s/' % \
             (AWS_STORAGE_BUCKET_NAME, STATIC_S3_PATH)
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+else:
+    STATIC_ROOT = 'static'
+    STATIC_URL = '/static/'
+    MEDIA_ROOT = 'media'
+    MEDIA_URL = '/media/'
 
 GOOGLE_ANALYTICS_MODEL = True
