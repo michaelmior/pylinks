e1b2c0129f33dd499f35115e04e909a99ab77657
Michael Mior <mmior@uwaterloo.ca>
2017-02-18 11:20:29 -0500

    Elminate unnecessary object filter for link check

--- pylinks/links/linklists.py
+++ pylinks/links/linklists.py
@@ -4,7 +4,6 @@ from pylinks.links.models import Link
 class LinkLinklist(Linklist):
     """ Class to let linkcheck app discover fields containing links """
     model = Link
-    object_filter = {}
     url_fields = ['url']
 
 linklists = {'Links': LinkLinklist}
