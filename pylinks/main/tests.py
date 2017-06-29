2a17b9fdb55806d6397f506066a2a7e8c480020b
Michael Mior <mmior@uwaterloo.ca>
2017-06-29 10:48:09 -0400

    Add simple admin test just so we catch breakage early

--- pylinks/main/tests.py
+++ pylinks/main/tests.py
@@ -7,3 +7,7 @@ class GoogleAnalyticsTests(TestCase):
         GoogleAnalytics(site_id=1, web_property_id='12345').save()
         response = self.client.get('/')
         self.assertContains(response, "_gaq.push(['_setAccount', '12345']);")
+
+class AdminTests(TestCase):
+    def test_admin_login_loads(self):
+        self.assertEqual(self.client.get('/admin/login/').status_code, 200)
