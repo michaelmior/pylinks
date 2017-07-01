df8aaba56d33e2d512f80a20a8c371192a7964ed
Michael Mior <mmior@uwaterloo.ca>
2017-07-01 18:22:36 -0400

    Use HTTPS for S3 URLs

--- pylinks/settings.py
+++ pylinks/settings.py
@@ -183,7 +183,6 @@ if os.environ.get('AWS_ACCESS_KEY_ID'):
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_S3_SECURE_URLS = False
     AWS_QUERYSTRING_AUTH = False
 
     # Avoid using subdomains
