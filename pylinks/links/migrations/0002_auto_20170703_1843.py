9f57e4e790e74bc7beef70320b02097469ee8ef4
Michael Mior <mmior@uwaterloo.ca>
2017-07-05 17:05:08 -0400

    Ensure Uploadcare can be disabled

--- pylinks/links/migrations/0002_auto_20170703_1843.py
+++ pylinks/links/migrations/0002_auto_20170703_1843.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+
 from django.db import models, migrations
-import pylinks.links.utils
+
+if not os.environ.get('UPLOADCARE_DISABLED', False):
+    from pylinks.links.utils import LinkFileField
+else:
+    from django.db.models import FileField as LinkFileField
 
 
 class Migration(migrations.Migration):
@@ -15,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='link',
             name='file',
-            field=pylinks.links.utils.LinkFileField(default=None, max_length=500, null=True, help_text=b'A file to be uploaded and linked to instead of the URL.', blank=True),
+            field=LinkFileField(default=None, max_length=500, null=True, help_text=b'A file to be uploaded and linked to instead of the URL.', blank=True),
             preserve_default=True,
         ),
     ]
