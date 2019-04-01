ee2525c52795acdeb374b820dfab528e700b772d
Michael Mior <mmior@cs.rit.edu>
2019-04-01 14:34:53 -0400

    Add messages context processor

--- pylinks/settings.py
+++ pylinks/settings.py
@@ -95,6 +95,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
             )
         }
     },
