566d963747136c44fd4c53f0d194b48098340d59
Michael Mior <mmior@mail.rit.edu>
2023-12-04 13:34:24 -0500

    Remove deprecated setting

--- pylinks/settings.py
+++ pylinks/settings.py
@@ -67,10 +67,6 @@ APPEND_SLASH = True
 # to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
